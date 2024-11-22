7280cac4dfb9289f5916565e4460c3a17ea19bd2
Graham Picard <grahampicard@gmail.com>
2024-11-21 21:39:42 -0500

    updates logic to be a bit more concise. adds names and images centered

--- src/app.d.ts
+++ src/app.d.ts
@@ -1,5 +1,29 @@
 // See https://svelte.dev/docs/kit/types#app.d.ts
 // for information about these interfaces
+
+
+interface Player {
+	batting_order: number;
+	player_id: string;
+	rating: number;
+	player_name: string;
+	headshot: string;
+	ratings: number;
+}
+
+interface TeamLineup {
+	LF: Player;
+	"2B": Player;
+	RF: Player;
+	"1B": Player;
+	"3B": Player;
+	SS: Player;
+	DH: Player;
+	CF: Player;
+	C: Player;
+	SP: Player;
+}
+
 declare global {
 	namespace App {
 		// interface Error {}
@@ -7,7 +31,10 @@ declare global {
 		// interface PageData {}
 		// interface PageState {}
 		// interface Platform {}
+		interface Player extends Player { }
+		interface TeamLineup extends TeamLineup { }
+
 	}
 }
 
-export {};
+export { Player, TeamLineup };
